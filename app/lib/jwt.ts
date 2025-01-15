21337cac1855c75265f6f5dfb8625fb82aca25e3
Ritul <ritulmamdapur17@gmail.com>
2025-01-15 16:51:41 +0530

    build fixed

--- app/lib/jwt.ts
+++ app/lib/jwt.ts
@@ -1,13 +1,11 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET!;
-
 
 export function signToken(payload: object): string {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 }
 
-export function verifyToken(token: string): any {
+export function verifyToken(token: string): string | object | null {
   return jwt.verify(token, JWT_SECRET);
 }
-
